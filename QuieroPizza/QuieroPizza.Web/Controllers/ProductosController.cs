f9a1d08d9ebd8c2a5ad74b8240ba6ccee960c049
BranlyBC <branly.flores@hotmail.com>
2021-06-16 08:49:36 -0600

    terminando video 7

--- QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -13,25 +13,8 @@ namespace QuieroPizza.Web.Controllers
         public ActionResult Index()
         {
 
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "Ivermectina";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "Suero";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Kit Tratamiento Maiz";
-
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return View(listadeProductos);
+            
+            return View();
         }
     }
 }
