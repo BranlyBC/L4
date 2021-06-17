1c9cf85c45dacef775501bd31fa3325ce7210083
BranlyBC <branly.flores@hotmail.com>
2021-06-16 20:48:10 -0600

    video 12 listo

--- QuieroPizza/QuieroPizza.BL/ProductosBL.cs
+++ QuieroPizza/QuieroPizza.BL/ProductosBL.cs
@@ -16,33 +16,12 @@ namespace QuieroPizza.BL
         }
 
 
-         public List<Producto> ObtenerProductos ()
+         public List<Producto> ObtenerProductos()
         {
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Ivermectina";
-            producto1.Precio = 150;
-       
+            
+            return _contexto.Productos.ToList();
 
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Suero";
-            producto2.Precio = 100;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Kit Tratamiento Maiz";
-            producto3.Precio = 1500;
-
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-
-
-            return listadeProductos;
+            
         }
     }
 }
